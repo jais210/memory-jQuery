274955b83d3646432f45861646f20df868bb380b
jais210 <jakeline_210@hotmail.com>
2017-08-29 17:08:00 -0500

    modificado con jQuery

--- main.js
+++ main.js
@@ -17,17 +17,17 @@
 
 // versión jQuery
 
-// var table = $('<table></table>').addClass('table')
-// 	for(i=0;i<3;i++){
-// 			var tr = $('<tr></tr>').addClass('tr').text(i);	
-// 		for(j=0;j<3;j++){
-// 			var td = $('<td></td>').addClass('td').text(j);
-// 			tr.append(td);
-// 		}
-// 	    table.append(tr);
-// 	}
+var table = $('<table></table>').addClass('table')
+for (i = 0; i < 3; i++) {
+    var tr = $('<tr></tr>').addClass('tr').text(i);
+    for (j = 0; j < 3; j++) {
+        var td = $('<td></td>').addClass('td').text(j);
+        tr.append(td);
+    }
+    table.append(tr);
+}
 
-// $('.tab').append(table);
+$('.tab').append(table);
 
 
 
