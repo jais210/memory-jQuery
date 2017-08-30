45209b74b280673c7272d81eade9ece8cf1780bd
jais210 <jackelineisabel.puruaya@gmail.com>
2017-08-30 08:44:52 -0500

    table

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
-// $(".juego").click(function(m) {
-//     var images = m.target.src;
-//     $(".conejo").append("#conejo")
-// })
-
-// var table = document.createElement('table').addClass('table');
-// 	for(i=0;i<3;i++){
-// 		var tr= document.createElement('tr').addClass('tr');
-
-// 		for(j=0;j<3;j++){
-// 			var td= document.createElement('td').addClass('td');
-// 			tr.appendChild(td);
-// 		}
-// 		table.appendChild(tr);		
-// 	}
-// $('.tab').append(table);
-
 // versión jQuery
 
 var table = $('<table></table>').addClass('table')
+
 for (i = 0; i < 3; i++) {
     var tr = $('<tr></tr>').addClass('tr').text(i);
-    for (j = 0; j < 3; j++) {
+        for (j = 0; j < 3; j++) {
         var td = $('<td></td>').addClass('td').text(j);
-        tr.append(td);
+    tr.append(td);
     }
-    table.append(tr);
+table.append(tr);
 }
 
-$('.tab').append(table);
+$('body').append(table);
 
 
 
